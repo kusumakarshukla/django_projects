a45e60b01d58a51a9910aa353423ed29c502a854
Kusumakar Shukla <kusumakar.shukla@gmail.com>
2022-10-03 23:09:06 +0400

    Delete NASA_DASHBOARD directory

--- NASA_DASHBOARD/asteroids/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import datetime
-# Create your views here.
-
-
-def home(request):
-    return render(request,"index.html",{"date":datetime.datetime.now()})
